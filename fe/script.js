dbc3702c41f3ba5aa8a75153e64ec06debf3f4bd
Iyusuf40 <96367786+Iyusuf40@users.noreply.github.com>
2023-03-05 23:27:16 +0000

    start of idea to project missing line

--- fe/script.js
+++ fe/script.js
@@ -1,6 +1,8 @@
 const canvas = document.getElementsByClassName('canvas')[0]
 
 let trackClick = 0
+let prevX = 0
+let prevY = 0
 
 canvas.addEventListener("mousedown", function(e) {
   trackClick = 1
@@ -8,6 +10,8 @@ canvas.addEventListener("mousedown", function(e) {
 
 canvas.addEventListener("mouseup", function(e) {
   trackClick = 0
+  prevX = 0
+  prevY = 0
 });
 
 canvas.addEventListener("mousemove", function(e) {
@@ -16,9 +20,17 @@ canvas.addEventListener("mousemove", function(e) {
   }
   const x = e.clientX
   const y = e.clientY
-  console.log(x, y)
+  if ((prevX || prevY) 
+    && (abs(prevX - x) > 5)
+    || (abs(prevY - y) > 5)) {
+      console.log(prevX - x, prevY - y)
+      // set flag to wait for 3rd position
+      // project line
+    }
   const el = write(`${x}px`, `${y}px`)
   canvas.appendChild(el)
+  prevX = x
+  prevY = y
 });
 
 function write(x, y) {
@@ -26,4 +38,9 @@ function write(x, y) {
   ink.style.left = x
   ink.style.top = y
   return ink
+}
+
+function abs(val) {
+  if (val < 0) return val * -1
+  return val
 }
