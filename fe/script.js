698e24bf4d44be14db92254a49d8c5720a35a759
Iyusuf40 <96367786+Iyusuf40@users.noreply.github.com>
2023-03-08 19:46:36 +0000

    add erase functionality to touch devices

--- fe/script.js
+++ fe/script.js
@@ -78,7 +78,8 @@ function handleTouchMove(e) {
 
   if (globalPoints.length > 1) drawPoints(canvas, globalPoints)
   } else if (drawOpts.mode === 'erase') {
-    const el = document.elementFromPoint(e.clientX, e.clientY)
+    const loc = e.originalEvent.changedTouches[0]
+    const el = document.elementFromPoint(loc.clientX, loc.clientY)
     if(el.getAttribute('data-pos')) {
       erase(el)
     }
