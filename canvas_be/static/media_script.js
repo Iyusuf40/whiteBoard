fd952db6552b8d28c672416f69e974ceb0c1bf23
Iyusuf40 <96367786+Iyusuf40@users.noreply.github.com>
2023-03-17 16:10:28 +0000

    incorporate media
    at this stage, media can be shared between peers.
    set css video grid position to fixed, top 0, right 0

--- canvas_be/static/media_script.js
+++ canvas_be/static/media_script.js
@@ -1,27 +1,23 @@
 const videoGrid = document.getElementById('video-grid')
 const audioToggle = document.getElementById('myBtn')
-const myPeer = new Peer()
 
 let localStream;
 
 const myVideo = document.createElement('video')
 myVideo.muted = true
 
-myPeer.on('open', id => {
-  socket.emit('join-room', ROOM_ID, id);
-})
-
 // remember to handle errors && also  put in a function
 async function userMedia() {
 	try {
-		navigator.mediaDevices.getUserMedia({
+		return navigator.mediaDevices.getUserMedia({
 			video: true,
 			audio: true,
 		}).then((stream) => {
 			setupMetadata(stream);
 			addVideoStream(myVideo, stream, undefined);
 			answerCall(stream);
-			userStatus(stream);
+            return stream
+			// userStatus(stream);
 		}).catch((err) => {
 			console.log(err);
 		})
@@ -30,23 +26,37 @@ async function userMedia() {
 	}
 }
 
-function userStatus(stream) {
-	socket.on('user-connected', userId => {
-		connectToNewUser(userId, stream);
-	})
-	socket.on('user-disconnected', userId => {
-		if (peers[userId]) peers[userId].close();
-	})
-}
-
 function setupMetadata(stream) {
-	localStream = stream;
+    localStream = stream
 	stream.getAudioTracks()[0].enabled = false;
 	stream.getVideoTracks()[0].enabled = false;
 	document.getElementById('myBtn').addEventListener("click", ToggleAudio)
 	document.getElementById('myBtn-v').addEventListener("click", ToggleVideo)
-
 };
+
+function addVideoStream(video, stream, call) {
+	video.srcObject = stream;
+	video.addEventListener('loadedmetadata', () => {
+		video.play();
+	})
+	videoGrid.append(video);
+	if (call !== undefined) {
+		call.on('close', () => {
+			video.remove();
+		});
+	}
+}
+
+function answerCall(stream) {
+	myPeer.on('call', call => {
+		call.answer(stream);
+		const video = document.createElement('video')
+		call.on('stream', userVideoStream => {
+			addVideoStream(video, userVideoStream, call);
+			console.log('Stream is called')
+		})
+	})
+}
 
 function ToggleVideo() {
 	if (localStream.getVideoTracks()[0].enabled) {
@@ -70,19 +80,8 @@ function ToggleAudio() {
 	}
 }
 
-function answerCall(stream) {
-	myPeer.on('call', call => {
-		call.answer(stream);
-		const video = document.createElement('video')
-		call.on('stream', userVideoStream => {
-			addVideoStream(video, userVideoStream, call);
-			console.log('Stream is called')
-		})
-	})
-	socket.emit('ready');
-}
 
-function connectToNewUser(userId, stream) {
+async function connectToNewUser(userId, stream) {
 	const call = myPeer.call(userId, stream);
 	const video = document.createElement('video');
 	call.on('error', (err) => {
@@ -90,23 +89,5 @@ function connectToNewUser(userId, stream) {
 	})
 	call.on('stream', userVideoStream => {
 		addVideoStream(video, userVideoStream, call);
-		console.log('created room user stream')
 	});
 }
-
-function addVideoStream(video, stream, call) {
-	video.srcObject = stream;
-	video.addEventListener('loadedmetadata', () => {
-		video.play();
-	})
-	videoGrid.append(video);
-	if (call !== undefined) {
-		peers[call.peer] = call;
-		call.on('close', () => {
-			video.remove();
-			delete peers[call.peer];
-		});
-	}
-}
-
-userMedia()
