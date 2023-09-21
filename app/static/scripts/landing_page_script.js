ee23024b930abd8ff0b76c192059bd2f770544a3
Iyusuf40 <96367786+Iyusuf40@users.noreply.github.com>
2023-09-21 11:32:06 +0000

    make responsive and add drawopts

--- app/static/scripts/landing_page_script.js
+++ app/static/scripts/landing_page_script.js
@@ -8,7 +8,7 @@ const blur = document.getElementById('blur')
 
 
 let key = null
-const baseUrl = 'http://localhost:3001/'
+const baseUrl = 'https://collab.cloza.org/'
 
 const postOpt = {
   method: "POST",
